2b77caa87a7c36416fb55ee26c493910a204d8ce
Robert Romano <bromano1992@gmail.com>
2022-07-24 03:36:52 -0400

    Installed ipython and defined str method on model for enhanced shell experience

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class React(models.Model):
 	name = models.CharField(max_length=500)
 	detail = models.CharField(max_length=500)
+
+	def __str__(self):
+			return f"The name is {self.name} and is {self.detail}"
