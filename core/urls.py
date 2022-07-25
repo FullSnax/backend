65febfbcbff5637ab2213a614cf90a923462a4bb
Robert Romano <bromano1992@gmail.com>
2022-07-25 18:58:05 -0400

    Setup router file for backend routing and confirmed everything still operating correctly

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path, include
 from . import views
 from core.views import *
-from rest_framework import routers
 from rest_framework import renderers
-
-router = routers.DefaultRouter()
-router.register(r'menuitems', MenuItemViewSet)
-
-
 
 urlpatterns = [
   path('profile/', ProfileView.as_view(), name='profile'),
-  # path('menuitem/', MenuItemView.as_view(), name='menu_item'),
   path('wel/', ReactView.as_view(), name="react"),
 ]
