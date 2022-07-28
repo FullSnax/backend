c1fa0196f1b72d29d6cab1d0c3c19b8c3446fce3
Robert Romano <bromano1992@gmail.com>
2022-07-28 10:15:40 -0400

    Setup setting py for auth

--- backend/urls.py
+++ backend/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('api/', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
