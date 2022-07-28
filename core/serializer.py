c9dacc1255af716c1737bbb351fe17785c416013
Robert Romano <bromano1992@gmail.com>
2022-07-28 11:20:26 -0400

    Implemented Register serializer with pw validation and user creation function

--- core/serializer.py
+++ core/serializer.py
@@ -24,6 +24,30 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         return token
       
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2')
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+      
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
       
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
