fa3102854aa2ab5170de4db8924c3b5ec6eb5d3a
Robert Romano <bromano1992@gmail.com>
2022-07-28 12:07:54 -0400

    Confirmed refresh tokens can be created and blacklisted on backend api

--- core/views.py
+++ core/views.py
@@ -1,17 +1,59 @@
 import profile
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from .models import *
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework import generics
 from rest_framework import renderers
+from core.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializer import *
+import base64
 from . import views
 from django.contrib.auth.models import User
 from datetime import timedelta
 
 # Create your views here.
 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+def b64e(s):
+    token = RefreshToken(base64_encoded_token_string)
+    return base64.b64encode(s.encode()).decode()
+    token.blacklist()
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+    
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/',
+        '/api/prediction/'
+    ]
+    return Response(routes)
+
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated,))
+def EndPoint(request):
+    if request.method == 'GET':
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = request.POST.get('text')
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
 
 class ProfileView(APIView):
 
